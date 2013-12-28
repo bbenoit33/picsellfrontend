7048f1b449c974585fb7a7d7607e0a3d4bd90c7c
James Rottinger <jamesrottinger@gmail.com>
2013-12-28 13:56:44 -0500

    added arrows

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("hello?");
+
+var recently_listed = $("#recentlyadded li.item_feature");
+for (element in recently_listed){
+	console.log(element);
+}
+var just_sold = $("#justsold li.item_feature");
