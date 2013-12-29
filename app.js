2f9aee63dcf37de211c210fb438ac55210178141
James Rottinger <jamesrottinger@gmail.com>
2013-12-29 01:57:40 -0500

    optimized images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-console.log("hello?");
+// console.log("hello?");
 
-var recently_listed = $("#recentlyadded li.item_feature");
-for (element in recently_listed){
-	console.log(element);
-}
-var just_sold = $("#justsold li.item_feature");
+// var recently_listed = $("#recentlyadded li.item_feature");
+// for (element in recently_listed){
+// 	console.log(element);
+// }
+// var just_sold = $("#justsold li.item_feature");
