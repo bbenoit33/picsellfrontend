b0830d19c4094a02abce092f6f75369b1461fa13
James Rottinger <jamesrottinger@gmail.com>
2013-12-30 14:35:03 -0500

    header work

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-$(function(){
-  $('#header_nav').data('size','big');
-});
-
-$(window).scroll(function(){
-  if($(document).scrollTop() > 0)
-{
-    if($('#header_nav').data('size') == 'big')
-    {
-        $('#header_nav').data('size','small');
-        $('#header_nav').stop().animate({
-            height:'40px'
-        },600);
-    }
-}
-else
-  {
-    if($('#header_nav').data('size') == 'small')
-      {
-        $('#header_nav').data('size','big');
-        $('#header_nav').stop().animate({
-            height:'100px'
-        },600);
-      }  
-  }
-});
+// $("header").on("click", ".hamburger_menu", function(event){
+// 	$("#dropdown_menu").slideToggle();
+// });
